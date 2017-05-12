49b0da0fe9611af435f6983ecded4ae7a3b01a02
Felipe Oliveira <felipe.oliveira@youse.com.br>
2017-05-12 15:13:11 -0300

    Remove byebug :wink:

--- lib/fibonacci.rb
+++ lib/fibonacci.rb
@@ -3,10 +3,9 @@ require 'byebug'
 class Fibonacci
 
   def self.seq(n)
-    return [] if n.zero?
+    return [0] if n.zero?
     return [1] if n == 1
 
-    byebug
     previous_value = seq(n - 1)
     previous_value + [previous_value[-2].to_i + previous_value[-1]]
   end
